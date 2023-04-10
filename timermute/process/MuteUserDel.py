026cdc6da28b8a274a146dece538427dad5e4997
shift <fantasy_world4423-git@yahoo.co.jp>
2023-04-11 08:02:33 +0900

    ログ出力機構WIP4

--- timermute/process/MuteUserDel.py
+++ timermute/process/MuteUserDel.py
@@ -15,7 +15,9 @@ class MuteUserDel(Base):
 
     def run(self, mw: MainWindowInfo) -> None:
         # "-MUTE_USER_DEL-"
+        logger.info("MUTE_USER_DEL -> start")
         # 選択ミュートユーザーを取得
+        logger.info("Getting selected mute user -> start")
         index_list = mw.values["-LIST_3-"]
         mute_user_list_all = mw.window["-LIST_3-"].get()
         mute_user_list = []
@@ -23,18 +25,26 @@ class MuteUserDel(Base):
             if i in index_list:
                 mute_user_list.append(mute_user)
         if not mute_user_list:
+            logger.info(f"Selected mute user is empty.")
+            logger.info("Getting selected mute user -> done")
+            logger.info("MUTE_USER_DEL -> done")
             return
+        logger.info("Getting selected mute user -> done")
 
         try:
             # ミュートユーザーをDBから削除
+            logger.info("DB delete -> start")
             for mute_user in mute_user_list:
                 mute_user_str = mute_user[1]
                 mw.mute_user_db.delete(mute_user_str)
+                logger.info(f"Deleted candidate mute user '{mute_user_str}'.")
+            logger.info("DB delete -> done")
         except Exception as e:
             raise e
         finally:
             # UI表示更新
             update_mute_user_table(mw.window, mw.mute_user_db)
+        logger.info("MUTE_WORD_DEL -> done")
         return
 
 
--- timermute/process/MuteUserUnmute.py
+++ timermute/process/MuteUserUnmute.py
@@ -15,8 +15,10 @@ class MuteUserUnmute(Base):
         pass
 
     def run(self, mw: MainWindowInfo) -> None:
-        # "-MUTE_User_UNMUTE-"
+        # "-MUTE_USER_UNMUTE-"
+        logger.info("MUTE_USER_UNMUTE -> start")
         # 選択ミュート済ユーザーを取得
+        logger.info("Getting selected muted user -> start")
         index_list = mw.values["-LIST_4-"]
         mute_user_list_all = mw.window["-LIST_4-"].get()
         mute_user_list = []
@@ -24,26 +26,37 @@ class MuteUserUnmute(Base):
             if i in index_list:
                 mute_user_list.append(mute_user)
         if not mute_user_list:
+            logger.info("Selected muted user is empty.")
+            logger.info("Getting selected mute user -> done")
+            logger.info("MUTE_WORD_UNMUTE -> done")
             return
+        logger.info("Getting selected muted user -> start")
 
         try:
             # Muter インスタンスを作成し、選択ユーザーのミュートを解除する
+            logger.info("Unmute by unmute_user -> start")
             config = mw.config
             screen_name = config["twitter"]["screen_name"]
             muter = Muter(screen_name)
             for mute_user in mute_user_list:
                 # 選択ユーザーのミュートを解除
                 mute_user_str = mute_user[1]
+                logger.info(f"Target user is '{mute_user_str}'.")
                 response = muter.unmute_user(mute_user_str)
                 print(response)
+                logger.info(f"'{mute_user_str}' is unmuted.")
 
                 # DB修正
+                logger.info("DB update -> start")
                 mw.mute_user_db.unmute(mute_user_str)
+                logger.info("DB update -> done")
+            logger.info("Unmute by unmute_user -> done")
         except Exception as e:
             raise e
         finally:
             # UI表示更新
             update_mute_user_table(mw.window, mw.mute_user_db)
+        logger.info("MUTE_USER_UNMUTE -> done")
         return
 
 
